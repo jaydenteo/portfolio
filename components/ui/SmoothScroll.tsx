dbe23901758de74872836ff4224f1d9775809a43
jaydenteo <jayden228@gmail.com>
2024-10-31 16:00:02 +0800

    test duration and easing

--- components/ui/SmoothScroll.tsx
+++ components/ui/SmoothScroll.tsx
@@ -10,8 +10,8 @@ type Props = {
 export default function SmoothScroll({ children }: Props) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 0.2,
-      easing: (t) => t,
+      duration: 0.5,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
     });
 
